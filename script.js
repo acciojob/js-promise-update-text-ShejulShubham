0543b9a7ee523583b4cb82f99a14bc64a2dae788
ShejulShubham <148786777+ShejulShubham@users.noreply.github.com>
2025-11-21 11:17:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const output = document.getElementById("output");
+
+function getPromise(){
+	return new Promise((response, reject)=>{
+		setTimeout(()=>{
+			response("Hello, world!")
+		}, 1000);
+	})
+}
+
+getPromise().then(response=>{
+	output.textContent = response;
+});
